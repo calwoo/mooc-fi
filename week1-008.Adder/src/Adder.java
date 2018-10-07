7d493219bb995f414e6b2113dc33a0ecdc07c9af
Calvin D. Woo <calvin.d.woo@gmail.com>
2018-10-07 10:32:42 -0400

    did more week 1

--- week1-008.Adder/src/Adder.java
+++ week1-008.Adder/src/Adder.java
@@ -7,5 +7,11 @@ public class Adder {
         Scanner reader = new Scanner(System.in);
 
         // Implement your program here. Remember to ask the input from user
+        System.out.println("Type a number: ");
+        int num = Integer.parseInt(reader.nextLine());
+        System.out.println("Type another number: ");
+        int num2 = Integer.parseInt(reader.nextLine());
+        int result = num + num2;
+        System.out.println("\nSum of the numbers: " + result);
     }
 }
--- week1-009.Divider/src/Divider.java
+++ week1-009.Divider/src/Divider.java
@@ -7,5 +7,12 @@ public class Divider {
         Scanner reader = new Scanner(System.in);
 
         // Implement your program here. Remember to ask the input from user.
+        System.out.println("Type a number: ");
+        int num = Integer.parseInt(reader.nextLine());
+        System.out.println("Type another number: ");
+        int num2 = Integer.parseInt(reader.nextLine());
+        double result = 1.0 * num / num2;
+        System.out.println("\nDivision: " + num + " / " + 
+                num2 + " = " + result);
     }
 }
