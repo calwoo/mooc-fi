869a7aeaf29b4f14bcca7bfc117c7f5eb78d3016
Calvin D. Woo <calvin.d.woo@gmail.com>
2018-10-07 18:08:11 -0400

    finished some exercises in week 3

--- week3-051.SeparatingCharacters/src/SeparatingCharacters.java
+++ week3-051.SeparatingCharacters/src/SeparatingCharacters.java
@@ -5,5 +5,13 @@ public class SeparatingCharacters {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type your name: ");
+        String name = reader.nextLine();
+        
+        int index = 1;
+        while (index <= name.length()) {
+            System.out.println(index + ". character: " + name.charAt(index - 1));
+            index++;
+        }
     }
 }
--- week3-052.ReversingName/src/ReversingName.java
+++ week3-052.ReversingName/src/ReversingName.java
@@ -3,5 +3,14 @@ import java.util.Scanner;
 public class ReversingName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type your name: ");
+        String name = reader.nextLine();
+        
+        int index = name.length() - 1;
+        System.out.print("In reverse order: ");
+        while (index >= 0) {
+            System.out.print(name.charAt(index));
+            index--;
+        }
     }
 }
