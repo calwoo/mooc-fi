41570dcb4be9c52bbe403cf78fb000b772fc0684
Calvin D. Woo <calvin.d.woo@gmail.com>
2018-10-08 16:11:57 -0400

    finished week 4 exercises

--- week4-081.PasswordRandomizer/src/PasswordRandomizer.java
+++ week4-081.PasswordRandomizer/src/PasswordRandomizer.java
@@ -2,13 +2,21 @@ import java.util.Random;
 
 public class PasswordRandomizer {
     // Define the variables
+    private int passLength;
+    private Random random = new Random();
 
     public PasswordRandomizer(int length) {
         // Initialize the variable
+        this.passLength = length;
     }
 
     public String createPassword() {
         // write code that returns a randomized password
-        return "";
+        String password = "";
+        for (int i = 0; i < this.passLength; i++) {
+            int num = random.nextInt(26);
+            password += "abcdefghijklmnopqrstuvwxyz".charAt(num);
+        }
+        return password;
     }
 }
