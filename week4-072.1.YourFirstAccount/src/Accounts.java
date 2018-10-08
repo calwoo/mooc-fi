ab3404c1c9736314a81699d809586831ff3a7423
Calvin D. Woo <calvin.d.woo@gmail.com>
2018-10-08 11:51:10 -0400

    more week 4 exercises

--- week4-072.1.YourFirstAccount/src/Accounts.java
+++ week4-072.1.YourFirstAccount/src/Accounts.java
@@ -6,6 +6,9 @@ public class Accounts {
     public static void main(String[] args) {
         // Code in Account.Java should not be touched!
         // write your code here
+        Account myAcct = new Account("Calvin", 100.0);
+        myAcct.deposit(20.0);
+        System.out.println(myAcct);
     }
 
 }
--- week4-072.2.YourFirstMoneyTransfer/src/Accounts.java
+++ week4-072.2.YourFirstMoneyTransfer/src/Accounts.java
@@ -4,6 +4,13 @@ public class Accounts {
     public static void main(String[] args) {
         // Code in Account.Java should not be touched!
         // write your code here
+        Account mattAcct = new Account("Matt's account", 1000.0);
+        Account myAcct = new Account("My account", 0.0);
+        
+        mattAcct.withdrawal(100.0);
+        myAcct.deposit(100.0);
+        System.out.println(mattAcct);
+        System.out.println(myAcct);
     }
 
 }
