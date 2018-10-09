4061b9e0e1e7d1a8f8e51fbf85cfda465de833db
Calvin D. Woo <calvin.d.woo@gmail.com>
2018-10-09 00:23:34 -0400

    finished up to exercise 100

--- week6-098.CopyAndReverse/src/Main.java
+++ week6-098.CopyAndReverse/src/Main.java
@@ -6,4 +6,22 @@ public class Main {
         // write testcode here
     }
     
+    public static int[] copy(int[] array) {
+        int length = array.length;
+        int[] copy = new int[length];
+        for (int i = 0; i < length; i++) {
+            copy[i] = array[i];
+        }
+        return copy;
+    }
+    
+    public static int[] reverseCopy(int[] array) {
+        int length = array.length;
+        int[] copy = new int[length];
+        for (int i = 0; i < length; i++) {
+            copy[i] = array[length - i - 1];
+        }
+        return copy;
+    }
+    
 }
